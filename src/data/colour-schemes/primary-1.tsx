9740c560e07249eb13319945a9ac4ffb864d9392
moveitroach <moveitroach@gmail.com>
2023-10-03 17:26:46 +1100

    add a less yellow and, simultaneously, less pink colour scheme

---
+++ src/data/colour-schemes/primary-1.tsx
@@ -0,0 +1,21 @@
+import ColourScheme from '@/models/colour-scheme'
+
+const primaryColourScheme: ColourScheme = {
+  backdropColour: '#F2E9D3',
+  alignedTextColour: '##462F16',
+  unalignedTextColour: '#F2EBCF',
+  primaryBackgroundColour: {
+    colour: '#FCF2DC',
+    aligned: true,
+  },
+  secondaryBackgroundColour: {
+    colour: '#6B5140',
+    aligned: false,
+  },
+  buttonBackgroundColour: {
+    colour: '#594436',
+    aligned: false,
+  },
+}
+
+export default primaryColourScheme
