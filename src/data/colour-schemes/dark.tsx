20221cb61b335518d98d8211563201e22b988fe9
James Wright <hello@jameswright.me>
2023-10-03 22:04:19 +1100

    Add quick test dark mode

---
+++ src/data/colour-schemes/dark.tsx
@@ -0,0 +1,21 @@
+import ColourScheme from '@/models/colour-scheme'
+
+const primaryColourScheme: ColourScheme = {
+  backdropColour: '#222',
+  alignedTextColour: '#fff',
+  unalignedTextColour: '#000',
+  primaryBackgroundColour: {
+    colour: '#111',
+    aligned: true,
+  },
+  secondaryBackgroundColour: {
+    colour: '#333',
+    aligned: true,
+  },
+  buttonBackgroundColour: {
+    colour: '#eee',
+    aligned: false,
+  },
+}
+
+export default primaryColourScheme
