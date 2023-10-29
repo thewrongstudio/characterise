a3c3a94b6e4642953150473b0fc4a21d755658d7
moveitroach <moveitroach@gmail.com>
2023-10-29 19:31:14 +1100

    Make the stat boxes but then...
    ...you realise input feilds are going to be hard and ugly

--- src/components/only-human-character-sheet/index.tsx
+++ src/components/only-human-character-sheet/index.tsx
@@ -3,27 +3,50 @@ import React, { useState } from 'react'
 import './styles.css'
 
 export default function OnlyHumanCharacterSheet() {
-  const [count, setCount] = useState(-1)
+  const [dexterity, setDexterity] = useState('0')
+  const [intelligence, setIntelligence] = useState('0')
   const [strength, setStrength] = useState('0')
+  const [will, setWill] = useState('0')
 
-  function handleClick() {
-    setCount(count + 1)
-  }
+  console.log(strength)
 
   return <div className='only-human-character-sheet'>
     <div>
-      <h1>This is a button you can click</h1>
-
-      <button onClick={handleClick}>
-        Clicked {count} times
-      </button>
-
-      <p>cat</p>
-      <input 
-        value={strength}
-        onChange={event => setStrength(event.target.value)}
+      <input
+        className='character-name'
       />
+      <div className='stats'>
+        <div>
+          <label>DEX</label>
+          <input
+            value={dexterity}
+            onChange={event => setDexterity(event.target.value)}
+          />
+        </div>
+        <div>
+          <label>INT</label>
+          <input 
+            value={intelligence}
+            onChange={event => setIntelligence(event.target.value)}
+          />
+        </div>
+        <div>
+          <label>STR</label>
+          <input 
+            value={strength}
+            onChange={event => setStrength(event.target.value)}
+          />
+        </div>
+        <div>
+          <label>WIL</label>
+          <input 
+            value={will}
+            onChange={event => setWill(event.target.value)}
+          />
+        </div>
+      </div>
     </div>
+
     <div className='right-panel'>
       <p>other cat</p>
     </div>
